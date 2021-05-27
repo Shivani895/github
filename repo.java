1bc96072e19b6e06dea8ee36d92ad0579b288b4b
Shivani Jaiswal <Shivani Jaiswal@DESKTOP-OTT105F>
2021-05-27 19:58:06 +0530

    First commit

---
+++ repo.java
@@ -0,0 +1,14 @@
+
+public class repo {
+	public static void main(String[] args)
+	{
+		String angry="yes";
+		if(angry=="yes")
+		{
+			System.out.println("its ok");
+		}
+		else
+		{
+			System.out.println("good");
+		}
+}
